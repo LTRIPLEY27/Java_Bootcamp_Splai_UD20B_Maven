1ef32f7351b8188b9e630dfd43c4f24e9c10ba77
IsaDevs <isabel.calzadilla.18@gmail.com>
2022-02-14 09:45:40 +0100

    maven start

---
+++ UD20B/src/main/java/com/UD20B/App.java
@@ -0,0 +1,13 @@
+package com.UD20B;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
